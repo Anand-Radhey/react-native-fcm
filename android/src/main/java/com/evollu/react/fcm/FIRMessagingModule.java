9f5d5a699b4a925e4f2b211e49ffbc09a3ec228c
Yogesh Chaudhary <yogesh.chaudhary@udaan.com>
2018-04-05 11:33:55 +0530

    fixing a NPE

--- android/src/main/java/com/evollu/react/fcm/FIRMessagingModule.java
+++ android/src/main/java/com/evollu/react/fcm/FIRMessagingModule.java
@@ -194,30 +194,31 @@ public class FIRMessagingModule extends ReactContextBaseJavaModule implements Li
             public void onReceive(Context context, Intent intent) {
             if (getReactApplicationContext().hasActiveCatalystInstance()) {
                 RemoteMessage message = intent.getParcelableExtra("data");
-                WritableMap params = Arguments.createMap();
-                WritableMap fcmData = Arguments.createMap();
+		if (message != null) {
+                        WritableMap params = Arguments.createMap();
+                        WritableMap fcmData = Arguments.createMap();
 
-                if (message.getNotification() != null) {
-                    Notification notification = message.getNotification();
-                    fcmData.putString("title", notification.getTitle());
-                    fcmData.putString("body", notification.getBody());
-                    fcmData.putString("color", notification.getColor());
-                    fcmData.putString("icon", notification.getIcon());
-                    fcmData.putString("tag", notification.getTag());
-                    fcmData.putString("action", notification.getClickAction());
-                }
-                params.putMap("fcm", fcmData);
+                        if (message.getNotification() != null) {
+                            Notification notification = message.getNotification();
+                            fcmData.putString("title", notification.getTitle());
+                            fcmData.putString("body", notification.getBody());
+                            fcmData.putString("color", notification.getColor());
+                            fcmData.putString("icon", notification.getIcon());
+                            fcmData.putString("tag", notification.getTag());
+                            fcmData.putString("action", notification.getClickAction());
+                        }
+                        params.putMap("fcm", fcmData);
 
-                if(message.getData() != null){
-                    Map<String, String> data = message.getData();
-                    Set<String> keysIterator = data.keySet();
-                    for(String key: keysIterator){
-                        params.putString(key, data.get(key));
-                    }
-                }
-                sendEvent("FCMNotificationReceived", params);
-
-            }
+                        if (message.getData() != null) {
+                            Map<String, String> data = message.getData();
+                            Set<String> keysIterator = data.keySet();
+                            for(String key: keysIterator){
+                                params.putString(key, data.get(key));
+                            }
+                        }
+                        sendEvent("FCMNotificationReceived", params);
+                 }
+              }
             }
         }, intentFilter);
     }
